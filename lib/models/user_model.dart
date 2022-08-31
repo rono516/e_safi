db3f84482f98e083c8ce15195bc3724b4f6271d7
Collins Rono <ronocollins2000@gmail.com>
2022-09-01 02:44:40 +0300

    register

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -2,14 +2,35 @@ class UserModel {
   String? email;
   String? wrool;
   String? uid;
+  String? fullName;
+  String? number;
+  String? location;
+  String? apartment;
+  int? houses;
+  DateTime? date;
 
 // receiving data
-  UserModel({this.uid, this.email, this.wrool});
+  UserModel(
+      {this.uid,
+      this.email,
+      this.wrool,
+      this.fullName,
+      this.number,
+      this.location,
+      this.apartment,
+      this.houses,
+      this.date});
   factory UserModel.fromMap(map) {
     return UserModel(
       uid: map['uid'],
       email: map['email'],
       wrool: map['wrool'],
+      apartment: map['apartment'],
+      date: map['date'],
+      fullName: map['fullName'],
+      location: map['location'],
+      houses: map['houses'],
+      number: map['number'],
     );
   }
 // sending data
@@ -18,6 +39,12 @@ class UserModel {
       'uid': uid,
       'email': email,
       'wrool': wrool,
+      'fullName': fullName,
+      'number': number,
+      'location': location,
+      'apartment': apartment,
+      'houses': houses,
+      'date': date
     };
   }
 }
--- lib/pages/collection_register.dart
+++ lib/pages/collection_register.dart
@@ -1,12 +1,14 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, unused_field, prefer_final_fields
 
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 // import '../widgets/exercise_title.dart';
 import 'package:intl/intl.dart';
+import 'package:wasteapp/models/user_model.dart';
 import 'package:wasteapp/pages/home_page.dart';
-import 'package:wasteapp/pages/login.dart';
-import 'package:wasteapp/pages/main_screen.dart';
 import 'dart:core';
+import 'package:firebase_auth/firebase_auth.dart';
 
 import 'package:wasteapp/values/height.dart';
 
@@ -18,10 +20,75 @@ class CollectionRegister extends StatefulWidget {
 }
 
 class _CollectionRegisterState extends State<CollectionRegister> {
-  TextEditingController apartmentController = TextEditingController();
+  final _numberFocusNode = FocusNode();
+  final _locationFocusNode = FocusNode();
+  final _apartmentFocusNode = FocusNode();
+  final _housesFocusNode = FocusNode();
+  //final _dateFocusNode = FocusNode();
   TextEditingController dateController = TextEditingController();
-  TextEditingController numberController = TextEditingController();
-  TextEditingController nameController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+  // final user = FirebaseAuth.instance.currentUser;
+  final _auth = FirebaseAuth.instance;
+  User? user = FirebaseAuth.instance.currentUser;
+  late String userId = user!.uid;
+  late String? userEmail = user!.email;
+
+  // var _userData = userModel.email = user.email
+
+  //final _userId = user!.uid;
+  var _userData = UserModel(
+      // uid: userId,
+      // email: user!.email,
+      // wrool: wrool,
+      fullName: null,
+      number: '',
+      location: '',
+      apartment: '',
+      houses: null,
+      date: null);
+
+  @override
+  void dispose() {
+    _numberFocusNode.dispose();
+    _locationFocusNode.dispose();
+    _apartmentFocusNode.dispose();
+    _housesFocusNode.dispose();
+    //_dateFocusNode.dispose();
+    super.dispose();
+  }
+
+  void _saveForm() async {
+    final isValid = _formKey.currentState!.validate();
+    if (!isValid) {
+      return;
+    }
+    _formKey.currentState!.save();
+    // print('fullName ${_userData.fullName}');
+    // print('number ${_userData.number}');
+    // print('location ${_userData.location}');
+    // print('apartment ${_userData.apartment}');
+    // print('houses ${_userData.houses}');
+    // print('Date ' +
+    //     DateFormat.yMMMMd('en_US')
+    //         .format(DateTime.parse(_userData.date.toString())));
+    FirebaseFirestore firebaseFirestore = FirebaseFirestore.instance;
+    User? user = _auth.currentUser;
+    UserModel userModel = UserModel();
+    // userModel.email = email;
+    //  userModel.uid = user!.uid;
+    userModel.fullName = _userData.fullName;
+    userModel.number = _userData.number;
+    userModel.location = _userData.location;
+    userModel.apartment = _userData.apartment;
+    userModel.houses = _userData.houses;
+    userModel.date = _userData.date;
+    // userModel.wrool = rool;
+    await firebaseFirestore
+        .collection("users")
+        .doc(user!.uid)
+        .set(userModel.toMap());
+    Navigator.pop(context);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,7 +97,7 @@ class _CollectionRegisterState extends State<CollectionRegister> {
       backgroundColor: Colors.green[800],
       body: SafeArea(
         child: Column(children: [
-          SizedBox(height: 25),
+          //  SizedBox(height: 25),
 
           //white area
           Expanded(
@@ -69,61 +136,168 @@ class _CollectionRegisterState extends State<CollectionRegister> {
                         child: Column(
                       children: <Widget>[
                         Form(
+                          key: _formKey,
                           child: Column(
                             // ignore: prefer_const_literals_to_create_immutables
                             children: [
                               TextFormField(
-                                  controller: nameController,
-                                  textInputAction: TextInputAction.next,
-                                  decoration: InputDecoration(
-                                    labelText: 'Full name',
-                                    hintText: 'Full name',
-                                    fillColor: Colors.white,
-                                  )),
-                              SizedBox(height: sizedboxheight),
-                              TextFormField(
-                                  controller: numberController,
-                                  textInputAction: TextInputAction.next,
-                                  keyboardType: TextInputType.number,
-                                  decoration: InputDecoration(
-                                    hintText: 'Your number',
-                                    fillColor: Colors.white,
-                                    labelText: 'Number',
-                                  )),
-                              SizedBox(height: sizedboxheight),
-                              TextFormField(
                                 textInputAction: TextInputAction.next,
+                                onFieldSubmitted: (_) {
+                                  FocusScope.of(context)
+                                      .requestFocus(_numberFocusNode);
+                                },
                                 decoration: InputDecoration(
-                                    labelText: 'Location',
-                                    hintText: 'Area of collection'),
+                                  labelText: 'Full name',
+                                  hintText: 'Full name',
+                                  fillColor: Colors.white,
+                                  errorStyle: TextStyle(fontSize: 15),
+                                ),
+                                validator: (value) {
+                                  if (value!.isEmpty) {
+                                    return 'Please enter your name';
+                                  }
+                                  return null;
+                                },
+                                onSaved: (value) {
+                                  _userData = UserModel(
+                                    fullName: value,
+                                    number: _userData.number,
+                                    location: _userData.location,
+                                    apartment: _userData.apartment,
+                                    houses: _userData.houses,
+                                    date: _userData.date,
+                                  );
+                                },
                               ),
                               SizedBox(height: sizedboxheight),
                               TextFormField(
-                                  controller: apartmentController,
-                                  textInputAction: TextInputAction.next,
-                                  decoration: InputDecoration(
-                                    hintText: 'Enter apartment',
-                                    labelText: 'Apartment',
-                                    fillColor: Colors.white,
-                                  )),
+                                textInputAction: TextInputAction.next,
+                                onFieldSubmitted: (_) {
+                                  FocusScope.of(context)
+                                      .requestFocus(_locationFocusNode);
+                                },
+                                keyboardType: TextInputType.number,
+                                focusNode: _numberFocusNode,
+                                decoration: InputDecoration(
+                                  hintText: 'Your number',
+                                  fillColor: Colors.white,
+                                  labelText: 'Number',
+                                ),
+                                validator: (value) {
+                                  if (value!.isEmpty) {
+                                    return 'Please enter your number';
+                                  }
+                                  return null;
+                                },
+                                onSaved: (value) {
+                                  _userData = UserModel(
+                                    fullName: _userData.fullName,
+                                    number: value,
+                                    location: _userData.location,
+                                    apartment: _userData.apartment,
+                                    houses: _userData.houses,
+                                    date: _userData.date,
+                                  );
+                                },
+                              ),
                               SizedBox(height: sizedboxheight),
                               TextFormField(
                                 textInputAction: TextInputAction.next,
+                                onFieldSubmitted: (_) {
+                                  FocusScope.of(context)
+                                      .requestFocus(_apartmentFocusNode);
+                                },
+                                focusNode: _locationFocusNode,
+                                decoration: InputDecoration(
+                                    labelText: 'Location',
+                                    hintText: 'Area of collection'),
+                                validator: (value) {
+                                  if (value!.isEmpty) {
+                                    return 'Please enter location';
+                                  }
+                                  return null;
+                                },
+                                onSaved: (value) {
+                                  _userData = UserModel(
+                                    fullName: _userData.fullName,
+                                    number: _userData.number,
+                                    location: value,
+                                    apartment: _userData.apartment,
+                                    houses: _userData.houses,
+                                    date: _userData.date,
+                                  );
+                                },
+                              ),
+                              SizedBox(height: sizedboxheight),
+                              TextFormField(
+                                //  controller: apartmentController,
+                                textInputAction: TextInputAction.next,
+                                onFieldSubmitted: (_) {
+                                  FocusScope.of(context)
+                                      .requestFocus(_housesFocusNode);
+                                },
+                                focusNode: _apartmentFocusNode,
+                                decoration: InputDecoration(
+                                  hintText: 'Enter apartment',
+                                  labelText: 'Apartment',
+                                  fillColor: Colors.white,
+                                ),
+                                validator: (value) {
+                                  if (value!.isEmpty) {
+                                    return 'Enter your apartment';
+                                  }
+                                  if (int.parse(value) <= 0) {
+                                    return 'Enter a number greater than zero';
+                                  }
+                                  return null;
+                                },
+                                onSaved: (value) {
+                                  _userData = UserModel(
+                                    fullName: _userData.fullName,
+                                    number: _userData.number,
+                                    location: _userData.location,
+                                    apartment: value,
+                                    houses: _userData.houses,
+                                    date: _userData.date,
+                                  );
+                                },
+                              ),
+                              SizedBox(height: sizedboxheight),
+                              TextFormField(
+                                textInputAction: TextInputAction.next,
+                                focusNode: _housesFocusNode,
                                 keyboardType: TextInputType.number,
                                 decoration: InputDecoration(
                                     labelText: 'Number of houses',
                                     hintText: 'No of houses'),
+                                validator: (value) {
+                                  if (value!.isEmpty) {
+                                    return 'Enter number of houses';
+                                  }
+                                  return null;
+                                },
+                                onSaved: (value) {
+                                  _userData = UserModel(
+                                    fullName: _userData.fullName,
+                                    number: _userData.number,
+                                    location: _userData.location,
+                                    apartment: _userData.apartment,
+                                    houses: int.parse(value!),
+                                    date: _userData.date,
+                                  );
+                                },
                               ),
                               SizedBox(height: sizedboxheight),
                               TextFormField(
                                 controller: dateController,
+                                textInputAction: TextInputAction.done,
                                 decoration: InputDecoration(
                                     labelText: 'Collection date',
                                     hintText: 'Enter collection date'),
                                 onTap: () async {
                                   DateTime? date = DateTime(1900);
                                   FocusScope.of(context)
-                                      .requestFocus(new FocusNode());
+                                      .requestFocus(FocusNode());
 
                                   date = await showDatePicker(
                                     context: context,
@@ -146,6 +320,16 @@ class _CollectionRegisterState extends State<CollectionRegister> {
                                   }
                                   return null;
                                 },
+                                onSaved: (value) {
+                                  _userData = UserModel(
+                                    fullName: _userData.fullName,
+                                    number: _userData.number,
+                                    location: _userData.location,
+                                    apartment: _userData.apartment,
+                                    houses: _userData.houses,
+                                    date: DateTime.parse(value!),
+                                  );
+                                },
                               ),
                               SizedBox(height: sizedboxheight),
                             ],
@@ -155,11 +339,20 @@ class _CollectionRegisterState extends State<CollectionRegister> {
                     )),
                     Container(
                       decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(8),
+                          borderRadius: BorderRadius.circular(10),
                           color: Colors.green),
                       width: double.infinity,
                       child: TextButton(
-                        onPressed: () {},
+                        onPressed: _saveForm,
+
+                        //     () {
+                        //   if (_formKey.currentState!.validate()) {
+                        //     //validate the form and show a dummy message || Registering you!
+                        //     ScaffoldMessenger.of(context)
+                        //         .showSnackBar(SnackBar(content: Text(userId)));
+                        //   }
+                        //   print(userId);
+                        // },
                         child: Text(
                           'SignUp for collection',
                           style: TextStyle(color: Colors.white),
